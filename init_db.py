c016b38c0c2d29ad036024671b685ead6317ebdf
Andrew Huezo <91641968+ahuezo005@users.noreply.github.com>
2025-04-09 20:46:16 -0700

    integrated bcrypt
    added bcrypt for hashed passwords
    
    added an additional column for course db --> time for course

--- init_db.py
+++ init_db.py
@@ -1,4 +1,4 @@
-from app import create_app, db
+from app import create_app, db, bcrypt #adding bcrypt for passwords
 
 app = create_app()
 
@@ -11,11 +11,30 @@ with app.app_context():
     db.create_all()
 
     # Add test users
-    student = User(username='sally', password='pass123', role='student')
-    teacher = User(username='tony', password='teach123', role='teacher')
-    admin = User(username='admin1', password='admin123', role='admin')
+    # student = User(username='sally', password='pass123', role='student')
+    # teacher = User(username='tony', password='teach123', role='teacher')
+    # admin = User(username='admin1', password='admin123', role='admin')
 
+    #hashed test users
+    hashed_pw_sally = bcrypt.generate_password_hash('pass123').decode('utf-8')
+    student = User(username='sally', password=hashed_pw_sally, role='student')
+    hashed_pw_santosh = bcrypt.generate_password_hash('teach123').decode('utf-8')
+    teacher = User(username='Santosh', password=hashed_pw_santosh, role='teacher')
+    hashed_pw_admin = bcrypt.generate_password_hash('admin123').decode('utf-8')
+    admin = User(username='admin1', password=hashed_pw_admin, role='admin')
     db.session.add_all([student, teacher, admin])
+    db.session.commit()
+
+    #test courses
+    course1 = Course(name="CSE 120", capacity=160, teacher_id=teacher.id, time="MW 4:30-5:45 PM")
+    course2 = Course(name="CSE 165", capacity=90, teacher_id=teacher.id, time="TR 11:00 12:15 PM")
+    db.session.add_all([course1, course2])
+    db.session.commit()
+
+    #test enrollments
+    enrolled1 = Enrollment(course_id=course1.id, user_id=student.id, grade="A")
+    enrolled2 = Enrollment(course_id=course2.id, user_id=student.id, grade="B")
+    db.session.add_all([enrolled1, enrolled2])
     db.session.commit()
 
     print("Database tables created and test users added!")
